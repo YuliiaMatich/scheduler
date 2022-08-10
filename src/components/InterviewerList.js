14fdb2c9a6e5591bb43e11e36cdfc9837f47ef99
YuliiaMatich <yuliia.matich@gmail.com>
2022-08-10 16:40:08 -0400

    refactor InterviewerList and InterviewerListItem components

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -7,15 +7,15 @@ export default function InterviewerList(props) {
 
   const interviewers = props.interviewers.map((interviewer) => {
     return (
-      <InterviewerListItem 
-      key={interviewer.id}
-      name={interviewer.name}
-      avatar={interviewer.avatar}
-      selected={interviewer.id === props.interviewer}
-      setInterviewer={() => props.setInterviewer(interviewer.id)}
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
       />
-    )
-  })
+    );
+  });
 
   return (
     <section className="interviewers">
